4e3c5c30d12889389f06b21c56b9a6f9157dee73
Kuba Firszt <firszt.kuba@gmail.com>
2022-10-03 21:08:17 +0200

    first commit

---
+++ tasks/urls.py
@@ -0,0 +1,10 @@
+import imp
+from unicodedata import name
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name="list"),
+    path('update_task/<int:pk>/', views.updateTask, name="update_task"),
+    path('delete_task/<int:pk>/', views.deleteTask, name="delete_task"),
+]
